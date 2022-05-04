f06ffb15d22cb0bcbf084c938a83d11641add7ca
raynapsimons <rayna_simons@brown.edu>
2022-05-04 14:52:52 -0400

    using new images?

--- videoshow.py
+++ videoshow.py
@@ -20,15 +20,19 @@ import os, glob
 ## 4. Get all the frames to show
 ## 5. Draw a dot into all the frames at the coordinate posList gives for each
 
-# frames = HelpFxns.folderToImgArray("/Users/rayna/Documents/GitHub/WormTrackV2/ClassifierOutput")
+framesforposlist = HelpFxns.folderToImgArray("/Users/rayna/Documents/GitHub/WormTrackV2/ClassifierInput")
+#INSTEAD: LOAD IN CSV JOHN IS MAKING
 
-ImgPath = "/Users/rayna/Documents/GitHub/WormTrackV2/ClassifierOutput"
+ImgPath = "/Users/rayna/Documents/GitHub/WormTrackV2/ClassifierInput"
 frames = glob.glob(os.path.join(ImgPath, '*.jpg'))
 frames.sort()
 
+positionlist = HelpFxns.getPosList(framesforposlist)
+print(positionlist[0])
 
 # make window and pixels
-win = visual.Window([800,800], color='blue', fullscr=0)
+win = visual.Window([1024,800], color='blue', fullscr=0)
+print('hello')
 
 
 
@@ -41,22 +45,33 @@ win = visual.Window([800,800], color='blue', fullscr=0)
     # win.flip() #flips the window to show it
 
 #show the images in order
-for f in range(0,len(frames),1):
-    c = visual.Circle(win, radius = 0.05, pos=(0,0), fillColor='red', lineColor=None) #eventually need to load in posititions
-    pic = visual.ImageStim(win, image=frames[f], colorSpace='rgb', size=0.9) #need to fix the size!
+for f in range(0,len(frames),5):
+    c = visual.Circle(win, radius = 0.01, pos=(0,0), fillColor='red', lineColor=None) #eventually need to load in positions
+    pic = visual.ImageStim(win, image=frames[f], colorSpace='rgb', size=2) #need to fix the size!
     pic.draw()
     c.draw()
     win.flip() #flips the window to show it
-    time.sleep(0.08)
+    time.sleep(0.07)
 
     
+#HALEY'S IDEA FOR FINDING COORDINATES OF SCREEN VS POSITION LIST:
+#print where it is drawing the dot each time in an approximate location for the first frame
 
-#plot the poslist
-## This block gets the list of x,y positions for each frame
-    #def getPosList(imgArray, stepSize='1'):
-        #stepSize=5
-        #poslist= np.zeros((listlen-1, 2)) #x,y
-        #poslist= np.empty((0,2), int)
+# x = -.5
+# y = .5
+
+# for s in range(1000):
+#     for ss in range(1000):
+#         xstep = s*.001
+#         ystep = ss*.001
+#         dot = visual.Circle(win,radius=.05,pos=(x+xstep,y+ystep),color='red')
+#         dot.draw()
+#         win.flip()
+#         print(x+xstep, y+ystep)
+#         time.sleep(.3)
+
+#print(1/0)
+
 
 input('exit')
 
